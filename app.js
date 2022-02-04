7b02452eed4d13131da982fb60bbe24e9b903f7c
Hannes Birgersson <hbirgersson@festina.com>
2022-02-04 15:31:11 +0100

    split file to app and handler

--- app.js
+++ app.js
@@ -1,124 +1,33 @@
-// TODO: Fix these disabled rules
-/* eslint-disable guard-for-in */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable camelcase */
 const express = require('express');
 const promiseRetry = require('promise-retry');
-
-const app = express();
-
 const tradfriLib = require('node-tradfri-client');
 const nodeCleanup = require('node-cleanup');
-const RGBColor = require('rgbcolor');
+
+const {
+  executeCommand,
+  deviceUpdated,
+  deviceRemoved,
+  groupUpdated,
+} = require('./tradfri_handler');
 
 // Copy envfile(copy_this).js and rename to envfile.js
 const {
   PORT, PASS, HUBIP, APIUSER, APIKEY,
 } = require('./envfile');
 
+const app = express();
+
 const { TradfriClient } = tradfriLib;
 
-// TODO: Don't use global mutable variables
-let tradfri = new TradfriClient(HUBIP);
-const groups = {};
-const lightbulbs = {};
+const tradfri = new TradfriClient(HUBIP);
 
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
-
-function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-}
-
-// eslint-disable-next-line no-unused-vars
-nodeCleanup((exitCode, signal) => {
+nodeCleanup(() => {
   console.log('Cleaning up...');
   if (tradfri) {
     console.log('Destroying tradfri connection');
     tradfri.destroy();
   }
-  tradfri = undefined;
 });
-
-function performOperation(bulb, command, state) {
-  console.log(command, bulb.name, `(${bulb.instanceId})`, state);
-  if (command === 'turn') {
-    tradfri.operateLight(bulb, { onOff: state === 'on' });
-  } else if (command === 'dim') {
-    tradfri.operateLight(bulb, { dimmer: state });
-  } else if (command === 'temp') {
-    tradfri.operateLight(bulb, { colorTemperature: state });
-  } else if (command === 'color') {
-    // Fixup color names into rgb value strings if necessary
-    const color = new RGBColor(state).toHex().slice(1);
-    tradfri.operateLight(bulb, { color });
-  }
-}
-
-const trimCommandString = (id) => {
-  // If it's just "all" then we set it to the empty string, so we fall back
-  // to setting every bulb (because every bulb name starts with empty string)
-  if (id === 'all') {
-    return '';
-  }
-  // When the id gets passed as "the living room" turn it into "living room"
-  // Also "all the" gets turned into all.
-  return replaceAll(id.replace(/the/g, '').trim().toLowerCase(), '_', ' ');
-};
-
-function executeCommand(idraw, command, state) {
-  const id = trimCommandString(idraw);
-
-  console.log('executeCommand', command, id, state);
-
-  for (const groupId in groups) {
-    const group = groups[groupId];
-    if (group.name.toLowerCase() === id) {
-      tradfri.operateGroup(group, { onOff: state === 'on' });
-      for (const deviceId of group.deviceIDs) {
-        const bulb = lightbulbs[deviceId];
-        if (bulb) { // skip non-bulbs
-          console.log('for group:, ', command, bulb.name, `(${bulb.instanceId})`, state);
-
-          performOperation(bulb, command, state);
-        }
-      }
-      return;
-    }
-  }
-
-  for (const bulbid in lightbulbs) {
-    const bulb = lightbulbs[bulbid];
-    if (bulb.name.toLowerCase().startsWith(id)) {
-      console.log('for bulbid: ', command, bulb.name, `(${bulb.instanceId})`, state);
-
-      performOperation(bulb, command, state);
-      // we don't return, so we can apply to all bulbs that share a naming convention
-    }
-  }
-}
-
-function tradfri_deviceUpdated(device) {
-  console.log('tradfri_deviceUpdated', device.instanceId, device.name);
-  if (device.type === tradfriLib.AccessoryTypes.lightbulb) {
-    // remember it
-    lightbulbs[device.instanceId] = device;
-  }
-}
-
-function tradfri_deviceRemoved(instanceId) {
-  if (instanceId in lightbulbs) {
-    console.log('tradfri_deviceRemoved', instanceId, lightbulbs[instanceId].name);
-    delete lightbulbs[instanceId];
-  }
-}
-
-function tradfri_groupUpdated(group) {
-  // remember it
-  console.log('tradfri_groupUpdated', group.instanceId, group.name);
-  groups[group.instanceId] = group;
-}
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -143,7 +52,7 @@ app.get('/api/:command/:id/:state', (req, res) => {
       || command === 'dim'
       || command === 'temp'
       || command === 'color') {
-    executeCommand(req.params.id, command, req.params.state);
+    executeCommand(tradfri, req.params.id, command, req.params.state);
     res.send('done');
     return;
   }
@@ -166,9 +75,9 @@ app.listen(PORT, async () => {
       throw err;
     }));
 
-  tradfri.on('device updated', tradfri_deviceUpdated)
-    .on('device removed', tradfri_deviceRemoved)
+  tradfri.on('device updated', deviceUpdated)
+    .on('device removed', deviceRemoved)
     .observeDevices();
-  tradfri.on('group updated', tradfri_groupUpdated)
+  tradfri.on('group updated', groupUpdated)
     .observeGroupsAndScenes();
 });
---
+++ tradfri_handler.js
@@ -0,0 +1,105 @@
+// TODO: Fix these disabled rules
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+const RGBColor = require('rgbcolor');
+const tradfriLib = require('node-tradfri-client');
+
+// TODO: Don't use global mutable variables
+const groups = {};
+const lightbulbs = {};
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+// eslint-disable-next-line no-unused-vars
+
+function performOperation(tradfri, bulb, command, state) {
+  console.log(command, bulb.name, `(${bulb.instanceId})`, state);
+  if (command === 'turn') {
+    tradfri.operateLight(bulb, { onOff: state === 'on' });
+  } else if (command === 'dim') {
+    tradfri.operateLight(bulb, { dimmer: state });
+  } else if (command === 'temp') {
+    tradfri.operateLight(bulb, { colorTemperature: state });
+  } else if (command === 'color') {
+    // Fixup color names into rgb value strings if necessary
+    const color = new RGBColor(state).toHex().slice(1);
+    tradfri.operateLight(bulb, { color });
+  }
+}
+
+const trimCommandString = (id) => {
+  // If it's just "all" then we set it to the empty string, so we fall back
+  // to setting every bulb (because every bulb name starts with empty string)
+  if (id === 'all') {
+    return '';
+  }
+  // When the id gets passed as "the living room" turn it into "living room"
+  // Also "all the" gets turned into all.
+  return replaceAll(id.replace(/the/g, '').trim().toLowerCase(), '_', ' ');
+};
+
+function executeCommand(tradfri, idraw, command, state) {
+  const id = trimCommandString(idraw);
+
+  console.log('executeCommand', command, id, state);
+
+  for (const groupId in groups) {
+    const group = groups[groupId];
+    if (group.name.toLowerCase() === id) {
+      tradfri.operateGroup(group, { onOff: state === 'on' });
+      for (const deviceId of group.deviceIDs) {
+        const bulb = lightbulbs[deviceId];
+        if (bulb) { // skip non-bulbs
+          console.log('for group:, ', command, bulb.name, `(${bulb.instanceId})`, state);
+
+          performOperation(tradfri, bulb, command, state);
+        }
+      }
+      return;
+    }
+  }
+
+  for (const bulbid in lightbulbs) {
+    const bulb = lightbulbs[bulbid];
+    if (bulb.name.toLowerCase().startsWith(id)) {
+      console.log('for bulbid: ', command, bulb.name, `(${bulb.instanceId})`, state);
+
+      performOperation(tradfri, bulb, command, state);
+      // we don't return, so we can apply to all bulbs that share a naming convention
+    }
+  }
+}
+
+function deviceUpdated(device) {
+  console.log('tradfri_deviceUpdated', device.instanceId, device.name);
+  if (device.type === tradfriLib.AccessoryTypes.lightbulb) {
+    // remember it
+    lightbulbs[device.instanceId] = device;
+  }
+}
+
+function deviceRemoved(instanceId) {
+  if (instanceId in lightbulbs) {
+    console.log('tradfri_deviceRemoved', instanceId, lightbulbs[instanceId].name);
+    delete lightbulbs[instanceId];
+  }
+}
+
+function groupUpdated(group) {
+  // remember it
+  console.log('tradfri_groupUpdated', group.instanceId, group.name);
+  groups[group.instanceId] = group;
+}
+
+module.exports = {
+  executeCommand,
+  deviceUpdated,
+  deviceRemoved,
+  groupUpdated,
+};
