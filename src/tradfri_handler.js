7a072e23b206c38b381a55680e043f997f23507e
Hannes Birgersson <hannes@birgersson.dev>
2022-02-10 22:17:28 +0100

    good progress, toggle induvidual devices working

--- src/tradfri_handler.js
+++ src/tradfri_handler.js
@@ -1,13 +1,9 @@
 // TODO: Fix these disabled rules
-/* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
 const RGBColor = require('rgbcolor');
 const tradfriLib = require('node-tradfri-client');
+const _ = require('lodash');
 
-// TODO: Don't use global mutable variables
-// const groups = {};
-// const devices = {};
-// const plugs = {}
 
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
@@ -17,28 +13,27 @@ function replaceAll(str, find, replace) {
   return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
-// eslint-disable-next-line no-unused-vars
+async function operate(tradfri, device, operation) {
+  if (device.type === tradfriLib.AccessoryTypes.lightbulb) {
+    tradfri.operateLight(device, operation)
+  } else if (device.type === tradfriLib.AccessoryTypes.plug && operation.onOff !== null) {
+    tradfri.operatePlug(device, operation)
+  }
+}
 
 function performOperation(tradfri, device, command, state) {
-  // console.log(device);
   const currentState = (device.lightList || device.plugList)[0].onOff;
-  console.log(
-    command, device.name, `(${device.instanceId})`, currentState ? 'on' : 'off', '>', state
-  );
+  console.log(command, device.name, `(${device.instanceId})`, currentState ? 'on' : 'off', '>', state);
   if (command === 'turn') {
-    if (state === 'toggle') {
-      tradfri.operateLight(device, { onOff: !currentState });
-    } else {
-      tradfri.operateLight(device, { onOff: state === 'on' });
-    }
+    operate(tradfri, device, { onOff: state === 'on' });
   } else if (command === 'dim') {
-    tradfri.operateLight(device, { dimmer: state });
+    operate(tradfri, device, { dimmer: state });
   } else if (command === 'temp') {
-    tradfri.operateLight(device, { colorTemperature: state });
+    operate(tradfri, device, { colorTemperature: state });
   } else if (command === 'color') {
     // Fixup color names into rgb value strings if necessary
     const color = new RGBColor(state).toHex().slice(1);
-    tradfri.operateLight(device, { color });
+    operate(tradfri, device, { color });
   }
 }
 
@@ -48,39 +43,43 @@ const trimCommandString = (id) => {
   if (id === 'all') {
     return '';
   }
-  // When the id gets passed as "the living room" turn it into "living room"
+  // When the id gets passed as "the_living_room" turn it into "living room"
   // Also "all the" gets turned into all.
   console.log(id);
   return replaceAll(id.replace(/the/g, '').trim().toLowerCase(), '_', ' ');
 };
 
-// function executeGroup()
-
 function executeCommand(tradfri, idRaw, command, state) {
-  const devices = tradfri.devices;
-  const groups = tradfri.groups;
+  const devices = _.pickBy(tradfri.devices, (device) =>
+    device.type === tradfriLib.AccessoryTypes.lightbulb
+    || device.type === tradfriLib.AccessoryTypes.plug);
+  const { groups } = tradfri;
+
   const id = trimCommandString(idRaw);
 
   console.log('executeCommand', command, id, state);
-  const groupMatch = Object.keys(groups).filter((group) =>  groups[group].group.name.toLowerCase().includes(id));
-  if (groupMatch.length > 0) {
-    tradfri.operateGroup(group, { onOff: state === 'on' });
-    console.log("for group:", groupId);
-    for (const deviceId of group.deviceIDs) {
-      const device = devices[deviceId];
-      if (device.type === tradfriLib.AccessoryTypes.lightbulb ||
-            device.type === tradfriLib.AccessoryTypes.plug) { // skip non-bulbs
-        performOperation(tradfri, device, command, state);
-      }
-    }
+  const groupMatch = Object.keys(groups)
+    .filter((group) => groups[group].group.name.toLowerCase().includes(id));
+
+  if (groupMatch.length >= 1 && groupMatch[0]) {
+    groupMatch.forEach( (groupId) => {
+      const group = groups[groupId].group;
+      // tradfri.operateGroup(group, { onOff: state === 'on' });
+      console.log('for group:', groupId);
+      group.deviceIDs.forEach((deviceId) => {
+        const device = devices[deviceId];
+        if (device) {
+          performOperation(tradfri, device, command, state);
+        }
+      })
+    });
     return;
   }
-  console.log(devices);
-  const deviceMatch = Object.keys(devices).filter((device) =>  devices[device].device.name.toLowerCase().startsWith(id));
+
+  const deviceMatch = Object.keys(devices).filter((device) => devices[device].name.toLowerCase().startsWith(id));
   for (const deviceID in deviceMatch) {
     console.log(deviceID);
     const device = devices[deviceID];
-
   }
 
   for (const bulbid in devices) {
